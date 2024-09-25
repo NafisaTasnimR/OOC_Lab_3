014dad30aa5b4b6c5cb30e1f40c887fe824f8c17
nafisa <nafisatasnim8855@gmail.com>
2024-09-26 03:07:22 +0600

    push function added.

--- src/Task2/MaxStack.java
+++ src/Task2/MaxStack.java
@@ -1,4 +1,28 @@
 package Task2;
 
+import java.util.Stack;
+
 public class MaxStack {
+    private Stack<Integer> MainStack;
+    private Stack<Integer> MinStack;
+    private Stack<Integer> MaxStack;
+
+    public MaxStack() {
+        MainStack = new Stack<>();
+        MinStack = new Stack<>();
+        MaxStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        MainStack.push(x);
+
+        if (MinStack.isEmpty() || x >= MinStack.peek()) {
+            MinStack.push(x);
+        }
+
+        if (MaxStack.isEmpty() || x <= MaxStack.peek()) {
+            MaxStack.push(x);
+        }
+    }
+
 }
